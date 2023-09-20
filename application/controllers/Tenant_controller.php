773dade3f21b9a8f07cc87f7969ff3df2fd9068c
artapamudaid <pamuda.id@gmail.com>
2023-09-20 16:11:13 +0700

    ubah tenant controller

--- application/controllers/Tenant_controller.php
+++ application/controllers/Tenant_controller.php
@@ -6,12 +6,21 @@ class Tenant_controller extends CI_Controller
     {
         parent::__construct();
 
+        //ambil username dari uri 1 (pertama)
         $username = $this->uri->segment(1);
 
+        //cek username
         if ($this->isValidClient($username)) {
+            //jika valid
+
+            //ambil data koneksi tenant
             $tenant = $this->tenantConfig($username);
             $tenant = json_decode($tenant, true);
 
+            //tutup koneksi landlord
+            $this->db->close();
+
+            //buat koneksi baru untuk akses db tenant
             $db_config = array(
                 'dsn'      => '',
                 'hostname' => $tenant['host'],
@@ -21,10 +30,10 @@ class Tenant_controller extends CI_Controller
                 'dbdriver' => 'mysqli'
             );
 
-            $this->db->close();
-
+            //load database tenant
             $this->load->database($db_config);
         } else {
+            //jika tenant tidak ditemukan maka error 404
             show_404();
         }
     }
